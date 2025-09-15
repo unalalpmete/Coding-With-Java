48091b953f15a76be8569c31ea7453decf7e42f0
Alp Mete Ünal <unalalpmete@gmail.com>
2025-09-15 12:58:16 +0300

    switchcasePractice3.java

---
+++ switch-case/switchcasePractice3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SwitchCasePractice3 {
+    public static void main(String[] args) {
+        // calculator with switch case
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("Choose operation (+, -, *, /): ");
+        char operation = input.next().charAt(0); 
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error = Division by zero!");
+                }
+                break;
+            default:
+                System.out.println("Invalid case");
+        }
+    }
+}
